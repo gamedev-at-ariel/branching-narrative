bf2715bc573d64ae876e385841fdc520fe188eb2
Erel Segal-Halevi <erelsgl@gmail.com>
2020-12-24 21:41:29 +0200

    Fix movement and change background

--- Assets/Scripts/StartConversation.cs
+++ Assets/Scripts/StartConversation.cs
@@ -1,58 +1,52 @@
-﻿using TMPro;
-using UnityEngine;
-using UnityEngine.UIElements;
+﻿using UnityEngine;
 
-public class StartConversation : MonoBehaviour
-{
-    [SerializeField]
-    private float rayLength;
-    [SerializeField]
-    private LayerMask layermask;
 
-    [SerializeField]
-    private GameObject ConversationPanel;
-    [SerializeField]
-    private GameObject background;
-    [SerializeField]
-    private GameObject TextConversation;
+public class StartConversation : MonoBehaviour {
+    [SerializeField] private float rayLength;
+    [SerializeField] private LayerMask layermask;
+
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject TextConversation;
+    [SerializeField] private KeyCode keyToStartConversation = KeyCode.E;
 
     NPC otherObject;
     bool isEnter = false;
     float distance;
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0)){
+    private void OpenConversation() {
+        isEnter = true;
+        background.SetActive(true);
+        TextConversation.SetActive(true);
+        otherObject.GoToPassage();
+        otherObject.Active();
+    }
+
+    private void CloseConversation() {
+        background.SetActive(false);
+        TextConversation.SetActive(false);
+        otherObject.ShotDown();
+        isEnter = false;
+    }
+
+    private void Start() {
+        CloseConversation();
+    }
+
+    private void Update()  {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(keyToStartConversation)) {
             RaycastHit hit;
             // Does the ray intersect any objects excluding the player layer
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayLength, layermask) && !isEnter)
-            {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayLength, layermask) && !isEnter) {
                 otherObject = hit.collider.gameObject.GetComponent<NPC>();
                 Debug.Log("hit " + otherObject.name);
-
-                // Calculate the distance between Player and NPC
-                distance = Vector3.Distance(otherObject.transform.position, transform.position);
-
-                //Open conversation
-                isEnter = true;
-                background.SetActive(true);
-                TextConversation.SetActive(true);
-                otherObject.GoToPassage();
-                otherObject.Active();
-            }    
+                OpenConversation();
+            }
         }
 
-        if (isEnter)
-        {
+        if (isEnter) {
             distance = Vector3.Distance(otherObject.transform.position, transform.position);
-            // If the player is too far away, close conversation
-            if (distance > rayLength)
-            {
-                background.SetActive(false);
-                TextConversation.SetActive(false);
-                otherObject.ShotDown();
-                isEnter = false;
-            }
+            if (distance > rayLength)              // If the player is too far away, close conversation
+                CloseConversation();
         }
     }
 }
